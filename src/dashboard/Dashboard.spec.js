e5e8202233342022bcb562db8bbb788f4328d388
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-06-19 15:47:52 -0700

    display test

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,12 @@
-// Test away
+import React from 'react';
+import {render} from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import Dashboard from './Dashboard'
+
+
+
+describe('renders without error', () => {
+    it('dashboard shows the control and display', () => {
+        render(<Dashboard />)
+    })
+})
